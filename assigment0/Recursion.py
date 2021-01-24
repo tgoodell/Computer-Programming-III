b129f376bdddaeae9660116ef47eda5158b00a90
Tristan Goodell <tgoodell@disroot.org>
2021-01-24 11:47:10 -0600

    6

--- assigment0/Recursion.py
+++ assigment0/Recursion.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
 import math
+import random
 
 DEBUG=True
 tx=3556 # 576
 ty=3681 # 499
+cost=0
 
 def show(img,title="image",wait=True):
     d=max(img.shape[:2])
@@ -30,154 +32,120 @@ def normalize(img):
     img_copy*=255.9999
     return np.uint8(img_copy)
 
-def safe(img,x,y):
-    if np.abs(img[x,y-1]-img[x,y])<=1 and np.abs(img[x+1,y]-img[x,y])<=1 and np.abs(img[x,y+1]-img[x,y])<=1 and np.abs(img[x-x,y]-img[x,y])<=1:
-        return True
+def flowDirt(img,cimg,x,y):
+    if img[y,x]<=208:
+        if cimg[y,x,2]<255:
+            cimg[y,x,2]+=1
+        img[y,x]+=1
+        cimg[y,x,0]+=1
+        cimg[y,x,1]+=1
+        # show(cimg, wait=False)
+        if y-1>0 and img[y-1,x]<img[y,x]-1:
+            img[y,x]-=1
+            cimg[y,x,0]-=1
+            cimg[y,x,1]-=1
+            flowDirt(img,cimg,x,y-1)
+        elif x+1<4095 and img[y,x+1]<img[y,x]-1:
+            img[y,x]-=1
+            cimg[y,x,0]-=1
+            cimg[y,x,1]-=1
+            flowDirt(img,cimg,x+1,y)
+        elif y+1<4095 and img[y+1,x]<img[y,x]-1:
+            img[y,x]-=1
+            cimg[y,x,0]-=1
+            cimg[y,x,1]-=1
+            flowDirt(img,cimg,x,y+1)
+        elif x-1>0 and img[y,x-1]<img[y,x]-1:
+            img[y,x]-=1
+            cimg[y,x,0]-=1
+            cimg[y,x,1]-=1
+            flowDirt(img,cimg,x-1,y)
+    elif y-1>0 and img[y-1,x]<img[y,x]-1:
+        flowDirt(img,cimg,x,y-1)
+    elif x+1<4095 and img[y,x+1]<img[y,x]-1:
+        flowDirt(img,cimg,x+1,y)
+    elif y+1<4095 and img[y+1,x]<img[y,x]-1:
+        flowDirt(img,cimg,x,y+1)
+    elif x-1>0 and img[y,x-1]<img[y,x]-1:
+        flowDirt(img,cimg,x-1,y)
 
-def northSafe(img,x,y):
-    compareValue=img[x,y]
-    if img[x,y-1]<=img[x,y]+1 and img[x,y-1]>=img[x,y]-1:
-        return True
-    else:
-        return False
-
-def flowingDirt(img,cimg):
+def dirtDriver(img,tx=3681,ty=3556):
+    cimg=cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
     n=0
-    patch=img[ty:ty+100,tx:tx+100]
-    maxV=np.max(patch)
-    while n<100:
+    cost=0
+    patch=img[ty:ty+400,tx:tx+400]
+    maxV=208 # np.max(patch)
+    print(maxV)
+    while n<401:
         k=0
-        while k<100:
-            x=tx+k
-            y=ty+n
-
-            while img[tx+k,ty+n]<maxV:
-                img[x,y]+=1
-                currentVal=img[x,y]
-                cimg[x,y,2]+=1
-                
-                if y-1>0 and img[x,y-1]<currentVal-1:
-                    img[x,y]-=1
-                    y-=1
-                if x+1<1024 and img[x+1,y]<currentVal-1:
-                    img[x,y]-=1
-                    x+=1
-                if y-1<1024 and img[x,y+1]<currentVal-1:
-                    img[x,y]-=1
-                    y+=1
-                if x-1>0 and img[x-1,y]<currentVal-1:
-                    img[x,y]-=1
-                    x-=1
-
-                show(cimg,wait=False)
-
-            if img[tx+k,ty+n]==maxV:
-                cimg[tx+k,ty+n,0]=255
-            show(cimg,wait=False)
+        while k<401:
+            while img[ty+n,tx+k]<maxV:
+                cost+=1
+                flowDirt(img,cimg,tx+k,ty+n)
+            if img[ty+n,tx+k]==maxV:
+                cimg[ty+n,tx+k,0]=255
+            print(str((((n)*0.0025+(k+1)/160000)*100)) + "%")
+            # show(cimg,wait=False)
             k+=1
         n+=1
 
-count=0
+    return img,cimg,cost
 
-def flowDirt(img,cimg,x,y):
-    if img[x,y]<=208:
-        if cimg[x,y,2]<255:
-            cimg[x,y,2]+=1
-        img[x,y]+=1
-        cimg[x,y,0]+=1
-        cimg[x,y,1]+=1
-        # show(cimg, wait=False)
-        if y>0 and img[x,y-1]<img[x,y]-1:
-            img[x,y]-=1
-            cimg[x,y,0]-=1
-            cimg[x,y,1]-=1
-            flowDirt(img,cimg,x,y-1)
-        elif x<4095 and img[x+1,y]<img[x,y]-1:
-            img[x,y]-=1
-            cimg[x,y,0]-=1
-            cimg[x,y,1]-=1
-            flowDirt(img,cimg,x+1,y)
-        elif y<4095 and img[x,y+1]<img[x,y]-1:
-            img[x,y]-=1
-            cimg[x,y,0]-=1
-            cimg[x,y,1]-=1
-            flowDirt(img,cimg,x,y+1)
-        elif x>0 and img[x-1,y]<img[x,y]-1:
-            img[x,y]-=1
-            cimg[x,y,0]-=1
-            cimg[x,y,1]-=1
-            flowDirt(img,cimg,x-1,y)
-    elif y>0 and img[x,y-1]<img[x,y]-1:
-        flowDirt(img,cimg,x,y-1)
-    elif x<4095 and img[x+1,y]<img[x,y]-1:
-        flowDirt(img,cimg,x+1,y)
-    elif y<4095 and img[x,y+1]<img[x,y]-1:
-        flowDirt(img,cimg,x,y+1)
-    elif x>0 and img[x-1,y]<img[x,y]-1:
-        flowDirt(img,cimg,x-1,y)
+def depositDirt(img,cimg,x,y,dugPixels):
+    count=0
+    while count<dugPixels:
+        rx=random.randrange(2,4095)
+        ry=random.randrange(2,4095)
+        randPixel=img[ry,rx]
 
+        if (rx>x and x<rx+400) or (ry>y and y<ry+400):
+            pass
+        elif randPixel==img[ry,rx-1]==img[ry-1,rx]==img[ry+1,rx]==img[ry,rx+1]:
+            img[ry,rx]+=1
+            cimg[ry,rx]+=1
+            cimg[ry,rx,1]=255
+            count+=1
+
+    return img,cimg
+
+def spotFinder(img):
+    candidates=[]
+
+    h,w=img.shape
+    nimg=cv2.resize(img,dsize=(w//10,h//10))
+
+    nh,nw=nimg.shape
+    n=0
+    while n<nh:
+        k=0
+        while k<nw:
+            candidates.append([k*10,n*10,np.std(nimg[n:n+40,k:k+40])])
+            k+=1
+        n+=1
+
+    np.sort(candidates)
+    for n in range(25):
+        print(candidates[n])
 
 
-oimg=cv2.imread("../input_erosion_safe.png",0)
-h,w=oimg.shape
 
-img=oimg # oimg[3*h//4:,3*w//4:]
-cv2.imwrite("cropped-out.png",img)
-print(oimg.shape)
-cimg=cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+img=cv2.imread("../input_erosion_safe.png",0)
+outImg=cv2.imread("out.png")
+outImg,cimg,cost=dirtDriver(img,2870,20)
 
-print(cimg.shape)
-# cimg[1000:1020,1000:1020,2]=255
-# cv2.imwrite("out.png",cimg)
+# 316451
 
-n=0
-patch=img[ty:ty+400,tx:tx+400]
-maxV=208 # np.max(patch)
-print(maxV)
-while n<401:
-    k=0
-    while k<401:
-        while img[tx+k,ty+n]<maxV:
-            flowDirt(img,cimg,tx+k,ty+n)
-        if img[tx+k,ty+n]==maxV:
-            cimg[tx+k,ty+n,0]=255
-        print(str((((n)*0.0025+(k+1)/160000)*100)) + "%")
-        # show(cimg,wait=False)
-        k+=1
-    n+=1
-
-# n=0
-# while n<451:
-#     k=0
-#     while k<451:
-#         x=tx-25+k
-#         y=ty-25+n
-#         if y>0 and img[x,y-1]<img[x,y]-1:
-#             print("False")
-#             flowDirt(img,cimg,x,y-1)
-#         if x<4095 and img[x+1,y]<img[x,y]-1:
-#             print("False")
-#             flowDirt(img,cimg,x+1,y)
-#         if y<4095 and img[x,y+1]<img[x,y]-1:
-#             print("False")
-#             flowDirt(img,cimg,x,y+1)
-#         if x>0 and img[x-1,y]<img[x,y]-1:
-#             print("False")
-#             flowDirt(img,cimg,x-1,y)
-#         k+=1
-#     n+=1
-
-# flowingDirt(img,cimg)
-
-patch=img[ty:ty+100,tx:tx+100]
-print(np.std(patch))
-print(np.average(patch))
-
-cost=np.sum(np.abs(img-oimg))
+# print(np.sum(outImg)-np.sum(img))
+print(np.sum(outImg))
+print(np.sum(img))
 print(cost)
-
-cv2.imwrite("out.png",img)
+print("Seward Cost: " + str(np.sum(np.abs(outImg-img))))
+cv2.imwrite("out.png",outImg)
 cv2.imwrite("colorOut.png",cimg)
+
+# cimg=cv2.imread("colorOut.png")
+
+# format: img[y,x]
 
 show(img,wait=True)
 show(cimg,wait=True)
