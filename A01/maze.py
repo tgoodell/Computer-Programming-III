5707d64771f58e51bf7241755a1d01858671fad9
Tristan Goodell <tgoodell@disroot.org>
2021-02-22 07:48:24 -0600

    21

---
+++ A01/maze.py
@@ -0,0 +1,115 @@
+import cv2
+import numpy as np
+import random
+import math
+
+DEBUG=True
+
+def show(img,title="image",wait=True):
+    d=max(img.shape[:2])
+    if d>1000:
+        step=int(math.ceil(d/1000))
+        img=img[::step,::step]
+    if not DEBUG:
+        return
+    if np.all(0<=img) and np.all(img<256):
+        cv2.imshow(title,np.uint8(img))
+    else:
+        cv2.imshow(title,normalize(img))
+    if wait:
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        cv2.waitKey(1)
+
+def normalize(img):
+    img_copy=img*1.0
+    img_copy-=np.min(img_copy)
+    img_copy/=np.max(img_copy)
+    img_copy*=255.9999
+    return np.uint8(img_copy)
+
+def genBoard(width,height):
+    w=width//20
+    h=height//20
+
+    board=np.zeros((h*2+1,w*2+1),dtype=np.uint8)
+    board[1::2,1::2]=254
+
+    return board
+
+def sewardMaze(width,height):
+    imgWidth=width//20
+    imgHeight=height//20
+    unexplored=254
+    explored=255
+
+    img=genBoard(width,height)
+
+    stack=[(1,1)]
+    img[1,1]=explored
+    count=0
+    while stack:
+        index=random.randrange(len(stack))*0+-1
+        x,y=stack[index]
+        possibles=[]
+        for dx,dy in (1,0),(-1,0),(0,1),(0,-1):
+            xp=x+2*dx
+            yp=y+2*dy
+
+            if 0<=xp<imgWidth and 0<=yp<imgHeight and img[yp,xp]==unexplored:
+                possibles.append((xp,yp))
+        if possibles:
+            xt,yt=random.choice(possibles)
+            img[yt,xt]=explored
+            img[(y+yt)//2,(x+xt)//2]=explored
+            stack.append((xt,yt))
+        else:
+            stack.pop(index)
+        # cv2.imwrite(f"animation/maze{count:05d}.png",img)
+        show(cv2.resize(img,dsize=(20*imgWidth,20*imgHeight),interpolation=cv2.INTER_AREA),wait=False)
+        count+=1
+        print(count)
+
+def sidewinder(width,height):
+    # 1.) Work through the grid row-wise, starting with the cell at 0,0. Initialize the “run” set to be empty.
+    # 2.) Add the current cell to the “run” set.
+    # 3.) For the current cell, randomly decide whether to carve east or not.
+    # 4.) If a passage was carved, make the new cell the current cell and repeat steps 2-4.
+    # 5.) If a passage was not carved, choose any one of the cells in the run set and carve a passage north. Then empty the run set, set the next cell in the row to be the current cell, and repeat steps 2-5.
+    # 6.) Continue until all rows have been processed.
+
+    board=genBoard(width,height)
+    cboard=cv2.cvtColor(board,cv2.COLOR_GRAY2BGR)
+
+    run=[]
+
+    h,w=board.shape
+
+    board[1,1:w-1]=255
+
+    k=3
+    while k<h:
+        n=1
+        while n<w-1:
+            run.append(n)
+            if random.randint(0,1)==1 and n<w-2:
+                # cboard[k,n:n+1,2]=255
+                board[k,n+1]=255
+            else:
+                first=run[0]
+                last=run[-1]
+                board[k-1,random.randint(first,last)]=255
+                # cboard=cv2.cvtColor(board,cv2.COLOR_GRAY2BGR)
+                run=[]
+            # show(cv2.resize(cboard, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=False)
+            n+=2
+        run=[]
+        k+=2
+
+    cv2.imwrite("maze.png",board)
+    show(cv2.resize(board, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=True)
+
+        
+sidewinder(1920,1080)
+
--- ic3Scraping.py
+++ ic3Scraping.py
@@ -5,6 +5,8 @@ import matplotlib.font_manager as fm
 from matplotlib.colors import ListedColormap
 import seaborn as sns
 from bs4 import BeautifulSoup
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
 
 # Array Format
     # Year - 2016,2017,2018,2019 - No longer a thing
@@ -22,6 +24,7 @@ yearLookup=["2016", "2017", "2018", "2019"]
 categoryLookup=["Victim Count", "Victim Loss", "Subject Count", "Subject Loss", "Age Group Count", "Age Group Amount Loss"]
 ageRangeLookup=["Under 20", "20 - 29", "30 - 39", "40 - 49", "50-59", "Over 60"]
 stateLookup=["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "US Minor Outlying Islands", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virgina", "Wisconsin", "Wyoming"]
+statesToCount=("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virgina", "Wisconsin", "Wyoming")
 statePopLookup=[4903185,731545,55114,7278717,3017804,39512223,5758736,3565287,973764,705749,21477737,10617423,169721,1415872,1787065,12671821,6732219,3155070,2913314,4467673,4648794,1344212,6045680,6892503,9986857,5639632,2976149,6137428,1068778,1934408,3080156,1359711,8882190,2096829,19453561,10488084,762062,57917,11689100,3956971,4217737,12801989,3193694,1059361,5148714,884659,6829174,28995881,300,3205958,623989,104328,8535519,7614893,1792147,5822434,578759]
 crimeLookup=["Advanced Fee", "Auction", "BEC/EAC", "Charity", "Civil Matter", "Confidence Fraud/Romance", "Corporate Data Breach", "Credit Card Fraud", "Crimes Against Children", "Criminal Forums", "Denial of Service/TDos", "Employment", "Extortion", "Gambling", "Government Impersonation", "Hacktivist", "Harassment/Threats of Violence", "Health Care Related", "IPR/Copyright and Counterfeit", "Identity Theft", "Investment", "Lottery/Sweepstakes/Inheritance", "Malware/Scareware/Virus", "Malware/Scareware", "Misrepresentation", "No Lead Value", "Non-payment/Non-Delivery", "Other", "Overpayment", "Personal Data Breach", "Phishing/Vishing/Smishing/Pharming", "Ransomware", "Re-shipping", "Real Estate/Rental", "Spoofing", "Tech Support", "Terrorism", "Virus", "Social Media", "Virtual Currency"]
 crimeLookup2019=["Advanced Fee", "BEC/EAC", "Charity", "Civil Matter", "Confidence Fraud/Romance", "Corporate Data Breach", "Credit Card Fraud", "Crimes Against Children", "Criminal Forums", "Denial of Service/TDos", "Employment", "Extortion", "Gambling", "Government Impersonation", "Hacktivist", "Harassment/Threats of Violence", "Health Care Related", "IPR/Copyright and Counterfeit", "Identity Theft", "Investment", "Lottery/Sweepstakes/Inheritance", "Malware/Scareware/Virus", "Misrepresentation", "No Lead Value", "Non-payment/Non-Delivery", "Other", "Overpayment", "Personal Data Breach", "Phishing/Vishing/Smishing/Pharming", "Ransomware", "Re-shipping", "Real Estate/Rental", "Spoofing", "Tech Support", "Terrorism", "Social Media", "Virtual Currency"]
@@ -168,22 +171,22 @@ def genScatterForAllYears(crime,category,filename="graph.png"):
     for year in yearLookup:
         if year=="2016":
             for state in stateLookup:
-                crimeSnippet.append(crimeData2016[stateLookup.index(state)][crimeLookup2016.index(crime)][categoryLookup.index(category)])
+                crimeSnippet.append(calculatePerHundredThousand("2016",state,crime,category))
                 years.append(year)
                 classes.append(state)
         elif year=="2017":
             for state in stateLookup:
-                crimeSnippet.append(crimeData2017[stateLookup.index(state)][crimeLookup2017.index(crime)][categoryLookup.index(category)])
+                crimeSnippet.append(calculatePerHundredThousand("2017", state, crime, category))
                 years.append(year)
                 classes.append(state)
         elif year=="2018":
             for state in stateLookup:
-                crimeSnippet.append(crimeData2018[stateLookup.index(state)][crimeLookup2018.index(crime)][categoryLookup.index(category)])
+                crimeSnippet.append(calculatePerHundredThousand("2018",state,crime,category))
                 years.append(year)
                 classes.append(state)
         elif year=="2019":
             for state in stateLookup:
-                crimeSnippet.append(crimeData2019[stateLookup.index(state)][crimeLookup2019.index(crime)][categoryLookup.index(category)])
+                crimeSnippet.append(calculatePerHundredThousand("2019",state,crime,category))
                 years.append(year)
                 classes.append(state)
 
@@ -195,15 +198,206 @@ def genScatterForAllYears(crime,category,filename="graph.png"):
     plt.ylabel(category)
     plt.savefig(filename)
 
+def genHorizontalBarGraph(year,crimez,category,filename="graph2.png"):
+    plt.rcdefaults()
+    states = statesToCount
+    crimeSnippet = []
+
+    for state in statesToCount:
+        sumState=0
+        for crime in crimeLookup2019:
+            sumState+=calculatePerHundredThousand(year, state, crime, category)
+        crimeSnippet.append(sumState / 4 * 10)
+
+    crimeSnippet,states=zip( *sorted( zip(crimeSnippet, states) ) )
+    crimeSnippet=crimeSnippet[::-1]
+    states=states[::-1]
+
+    # df = pd.DataFrame({'statez': states, 'y_poz': crimeSnippet})
+    # df.sort_values('y_poz', inplace=True)
+    plt.subplots(figsize=(19, 11))
+
+    plt.barh(states, crimeSnippet)
+    plt.title("Estimated " + str(crime) + " " + str(category) + " in the United States" + " - " + year, fontsize=20)
+    plt.ylabel("States")
+    plt.xlabel(category + " per 100,000 People")
+    plt.savefig(filename)
+
 def calculatePerHundredThousand(year,state,crime,category):
     return getDataPiece(year,state,crime,category)/statePopLookup[stateLookup.index(state)]*100000
+
+def genAllBarCharts(year):
+    for crime in crimeLookup:
+        genHorizontalBarGraph(year, crime, "Victim Count",filename="graphs/" + str(crime).replace("/", "-") + "-graph.png")
+        print(crime)
 
 # crimeData2016,crimeData2017,crimeData2018,crimeData2019=loadAllData()
 # genChartForAllYears("Identity Theft","Victim Loss")
 
+def printReport(year,crime,category):
+    states = statesToCount
+    crimeSnippet = []
+
+    for state in statesToCount:
+        sumState = 0
+        for crime in crimeLookup2019:
+            sumState += calculatePerHundredThousand(year, state, crime, category)
+        crimeSnippet.append(sumState / 4 * 10)
+
+    crimeSnippet,states=zip( *sorted( zip(crimeSnippet, states) ) )
+    crimeSnippet=crimeSnippet[::-1]
+    states=states[::-1]
+
+    for state in states:
+        print(state + ": " + str(crimeSnippet[states.index(state)]))
+
 crimeData2016,crimeData2017,crimeData2018,crimeData2019=loadAllData()
 
+crimeSnippet=[]
+for state in statesToCount:
+    sumState=0
+    for crime in crimeLookup2019:
+        sumState += calculatePerHundredThousand("2019", state, crime, "Victim Count")
+    crimeSnippet.append(sumState/2*5)
 
+states=statesToCount
+
+crimeSnippet,states=zip( *sorted( zip(crimeSnippet, states) ) )
+crimeSnippet=crimeSnippet[::-1]
+states=states[::-1]
+
+for state in states:
+    print(state + ", " + str(crimeSnippet[states.index(state)]))
+
+plt.subplots(figsize=(19, 11))
+
+plt.barh(states, crimeSnippet)
+plt.title("Estimated Cybercrime in the United States, 2019", fontsize=20)
+plt.ylabel("States")
+plt.xlabel("Victim Count per 100,000 People")
+plt.savefig("all-graph.png")
+
+print("\subsection{State-level Cybercrime Data}\n")
+print("\\begin{center}")
+print("\\begin{tabular}{|l|l|}")
+print("\hline")
+print("State & Estimated Victim Count in 2019 PHT \\\ \hline")
+
+for state in states:
+    print(state + " & " + str(crimeSnippet[states.index(state)]) + " \\\ \hline")
+print("\end{tabular}")
+print("\end{center}")
+print("\n\\newpage")
+
+# volume=crimeData2019
+#
+# x = np.arange(volume.shape[0])[:, None, None]
+# y = np.arange(volume.shape[1])[None, :, None]
+# z = np.arange(volume.shape[2])[None, None, :]
+# x, y, z = np.broadcast_arrays(x, y, z)
+
+# Turn the volumetric data into an RGB array that's
+# just grayscale.  There might be better ways to make
+# ax.scatter happy.
+
+# # Do the plotting in a single call.
+# fig = plt.figure(figsize=(19, 11))
+# ax = fig.gca(projection='3d')
+# ax.scatter(x.ravel(),y.ravel(),z.ravel())
+#
+# plt.title("2019 Cybercrime Data Array Format",fontsize=20)
+# plt.ylabel("Cybercrimes",fontsize=10)
+# plt.xlabel("States",fontsize=10)
+# ax.set_zlabel("Category",fontsize=10)
+#
+# plt.savefig("3dgraph.png")
+
+
+
+# printReport("2019","Confidence Fraud/Romance", "Victim Count")
+#
+# print(len(crimeLookup2019))
+
+# if True:
+#     plt.rcdefaults()
+#     states = statesToCount
+#     crimeSnippet = []
+#
+#     for state in statesToCount:
+#         sumState = 0
+#         for crime in crimeLookup2019:
+#             sumState += calculatePerHundredThousand("2019", state, crime, "Victim Loss")
+#         crimeSnippet.append(sumState / 4 * 10)
+#
+#     crimeSnippet,states=zip( *sorted( zip(crimeSnippet, states) ) )
+#     crimeSnippet=crimeSnippet[::-1]
+#     states=states[::-1]
+#
+#     # df = pd.DataFrame({'statez': states, 'y_poz': crimeSnippet})
+#     # df.sort_values('y_poz', inplace=True)
+#     plt.subplots(figsize=(19, 11))
+#
+#     plt.barh(states, crimeSnippet)
+#     plt.title("Estimated " + str("All Crime") + " " + str("Victim Loss") + " in the United States" + " - " + "2019", fontsize=20)
+#     plt.ylabel("States")
+#     plt.xlabel("Victim Loss" + " per 100,000 People")
+#     plt.savefig("graph3.png")
+
+# print("\subsection{Charts}")
+# for crime in crimeLookup2019:
+#     print("\subsubsection{" + crime + "}\n")
+#     print("\\begin{figure}[h!]")
+#     print("\centering")
+#     print("\includegraphics[width=1\\textwidth]{bar-charts/" + str(crime).replace("/", "-") + "-graph.png}")
+#     print("\caption{Estimated " + crime + " Cybercrime in 2019}")
+
+# for crime in crimeLookup2019:
+#     print("\subsection{" + crime + "}\n")
+#     print("\\begin{center}")
+#     print("\\begin{tabular}{|l|l|}")
+#     print("\hline")
+#     print("State & Estimated " + crime + " Victim Count in 2019 PHT \\\ \hline")
+#     crimeSnippet = []
+#     states = statesToCount
+#     for state in statesToCount:
+#         crimeSnippet.append(calculatePerHundredThousand("2019", state, crime, "Victim Count")/4*10)
+#     crimeSnippet, states = zip(*sorted(zip(crimeSnippet, states)))
+#     crimeSnippet = crimeSnippet[::-1]
+#     states = states[::-1]
+#     for state in states:
+#         print(state + " & " + str(crimeSnippet[states.index(state)]) + " \\\ \hline")
+#     print("\end{tabular}")
+#     print("\end{center}")
+#     print("\n\\newpage")
+
+# am=calculatePerHundredThousand("2019","Arkansas","Identity Theft","Victim Count")
+#
+# print(am)
+# print(am/4*10*statePopLookup[stateLookup.index("Arkansas")]/100000)
+# print(getDataPiece("2019","Arkansas","Identity Theft","Victim Count"))
+
+# labels = crimeLookup2019
+# sizes = np.zeros((len(crimeLookup2019)))
+# colors = (len(crimeLookup2019))*"red"
+# explode=[]
+#
+# for state in statesToCount:
+#     n=0
+#     while n<len(crimeLookup2019):
+#         sizes[n]+=crimeData2019[stateLookup.index(state)][n][0]
+#         n+=1
+#
+# for n in range(len(crimeLookup2019)):
+#     explode.append(0.1)
+#
+# # Plot
+# plt.subplots(figsize=(19, 11))
+# plt.pie(sizes, shadow=False, startangle=90, labels=labels,autopct='%1.1f%%',explode=explode)
+# # plt.legend(patches, labels, loc="best")
+# plt.title('Population Density Index')
+#
+# plt.axis('equal')
+# plt.savefig("pie.png")
 
 
 
