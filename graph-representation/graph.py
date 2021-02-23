4ac012faec517c5f69a3625fde6c51c99f8720d9
Laptop <tgoodell@disroot.org>
2021-02-23 10:10:27 -0600

    23

--- graph-representation/graph.py
+++ graph-representation/graph.py
@@ -4,8 +4,8 @@ s="""0 1 7
 1 2 10
 3 2 2
 1 4 15
-3 4 11
-2 5 9
+2 4 11
+3 5 9
 5 4 6"""
 
 adjList={}
@@ -15,7 +15,16 @@ for line in s.split("\n"):
     weight=[a,b,c]
     adjList[a]=adjList.get(a,[])+[(b,weight)]
     adjList[b]=adjList.get(b,[])+[(a,weight)]
-print(adjList)
-m=adjList[0][1][1]
-m[0]=234234
-print(adjList)
+# print(adjList)
+# Dikstra Pseudo Algorthm
+
+bestSoFar={0:0}
+queue=[0]
+while queue:
+    node=queue.pop(0) # get the smallest
+    for node2,(edgeCost,) in adjList[node]:
+        c=edgeCost+bestSoFar[node]
+        if node2 not in bestSoFar or bestSoFar[node2]>c:
+            bestSoFar[node2]=c
+            queue+=node2,
+print(bestSoFar)
