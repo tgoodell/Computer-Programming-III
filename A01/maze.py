3484e795d8a075d8b122e717c74163e3ebcfd89a
Tristan Goodell <tgoodell@disroot.org>
2021-02-25 21:13:54 -0600

    29

--- A01/maze.py
+++ A01/maze.py
@@ -38,17 +38,21 @@ def genBoard(width,height):
 
     return board
 
-def sewardMaze(width,height):
-    imgWidth=width//20
-    imgHeight=height//20
+def recursiveBacktracking(width,height):
     unexplored=254
     explored=255
 
-    img=genBoard(width,height)
+    maze=genBoard(width,height)
+    cmaze=cv2.cvtColor(maze,cv2.COLOR_GRAY2BGR)
 
+    darkRed=(12,20,146)
+    copper=(77,124,190)
+
+    mazeHeight,mazeWidth=maze.shape
     stack=[(1,1)]
-    img[1,1]=explored
+    maze[1,1]=explored
     count=0
+    video = cv2.VideoWriter("rb-maze.avi", cv2.VideoWriter_fourcc(*"MJPG"), 60, (1920, 1080))
     while stack:
         index=random.randrange(len(stack))*0+-1
         x,y=stack[index]
@@ -57,19 +61,29 @@ def sewardMaze(width,height):
             xp=x+2*dx
             yp=y+2*dy
 
-            if 0<=xp<imgWidth and 0<=yp<imgHeight and img[yp,xp]==unexplored:
+            if 0<=xp<mazeWidth and 0<=yp<mazeHeight and maze[yp,xp]==unexplored:
                 possibles.append((xp,yp))
         if possibles:
             xt,yt=random.choice(possibles)
-            img[yt,xt]=explored
-            img[(y+yt)//2,(x+xt)//2]=explored
+            maze[yt,xt]=explored
+            cmaze[yt,xt,:]=darkRed
+            maze[(y+yt)//2,(x+xt)//2]=explored
+            cmaze[(y+yt)//2,(x+xt)//2,:]=darkRed
             stack.append((xt,yt))
         else:
+            x,y=stack[index]
+            cmaze[y,x,:]=copper
             stack.pop(index)
         # cv2.imwrite(f"animation/maze{count:05d}.png",img)
-        show(cv2.resize(img,dsize=(20*imgWidth,20*imgHeight),interpolation=cv2.INTER_AREA),wait=False)
+        frame=cv2.resize(cmaze, dsize=(1920, 1080), interpolation=cv2.INTER_AREA)
+        video.write(frame)
         count+=1
-        print(count)
+        if count%100==0:
+            print(count)
+            # show(cv2.resize(cmaze, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=False)
+
+    show(cv2.resize(cmaze, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=True)
+    cv2.imwrite("rb-maze.png",maze)
 
 # Recursive Backtracking
 # Right/Left hand Maze Following
@@ -78,21 +92,19 @@ def sewardMaze(width,height):
 
 
 def sidewinder(width,height):
-    # 1.) Work through the grid row-wise, starting with the cell at 0,0. Initialize the “run” set to be empty.
-    # 2.) Add the current cell to the “run” set.
-    # 3.) For the current cell, randomly decide whether to carve east or not.
-    # 4.) If a passage was carved, make the new cell the current cell and repeat steps 2-4.
-    # 5.) If a passage was not carved, choose any one of the cells in the run set and carve a passage north. Then empty the run set, set the next cell in the row to be the current cell, and repeat steps 2-5.
-    # 6.) Continue until all rows have been processed.
-
     board=genBoard(width,height)
     cboard=cv2.cvtColor(board,cv2.COLOR_GRAY2BGR)
+    video = cv2.VideoWriter("sw-maze.avi", cv2.VideoWriter_fourcc(*"MJPG"), 30, (1920, 1080))
+
+    blueCrayola=(217,142,5)
+    pistachio=(109,190,144)
 
     run=[]
 
     h,w=board.shape
 
     board[1,1:w-1]=255
+    cboard[1,1:w-1,:]=blueCrayola
 
     k=3
     while k<h:
@@ -100,12 +112,14 @@ def sidewinder(width,height):
         while n<w-1:
             run.append(n)
             if random.randint(0,1)==1 and n<w-2:
-                cboard[k,n:n+1,2]=255
+                cboard[k,n,:]=blueCrayola
+                cboard[k,n+1,:]=blueCrayola
                 board[k,n+1]=255
                 # show(cv2.resize(cboard, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=False)
             else:
                 first=run[0]
                 last=run[-1]
+                cboard[k,n,:]=blueCrayola
 
                 # if board[k-2,random.randint(first,last)]==254:
                 badRand=True
@@ -113,16 +127,18 @@ def sidewinder(width,height):
                     place=random.randint(first,last)
                     if place%2==1:
                         board[k-1,place]=255
+                        cboard[k-1,place]=pistachio
                         badRand=False
                 # show(cv2.resize(cboard, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=False)
 
-                cboard=cv2.cvtColor(board,cv2.COLOR_GRAY2BGR)
                 run=[]
+            frame=cv2.resize(cboard, dsize=(1920, 1080), interpolation=cv2.INTER_AREA)
+            video.write(frame)
             n+=2
         run=[]
         k+=2
 
-    cv2.imwrite("maze.png",board)
+    cv2.imwrite("sw-maze.png",board)
     # show(cv2.resize(board, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=True)
 
     return board
@@ -158,31 +174,114 @@ def workerRB(board,x,y):
     else:
         return board
 
-def recursiveBacktracking(width,height):
-    # 1.) Choose a starting point in the field.
-    # 2.) Randomly choose a wall at that point and carve a passage through to the adjacent cell, but only if the adjacent cell has not been visited yet. This becomes the new current cell.
-    # 3.) If all adjacent cells have been visited, back up to the last cell that has uncarved walls and repeat.
-    # 4.) The algorithm ends when the process has backed all the way up to the starting point.
+def leftFree(dir,maze,x,y):
+    if dir==0 and maze[y,x-1]>0:
+        return True
+    elif dir==1 and maze[y-1,x]>0:
+        return True
+    elif dir==2 and maze[y,x+1]>0:
+        return True
+    elif dir==3 and maze[y+1,x]>0:
+        return True
+    else:
+        return False
+    
+def rightFree(dir,maze,x,y):
+    if dir==0 and maze[y,x+1]>0:
+        return True
+    elif dir==1 and maze[y+1,x]>0:
+        return True
+    elif dir==2 and maze[y,x-1]>0:
+        return True
+    elif dir==3 and maze[y-1,x]>0:
+        return True
+    else:
+        return False
 
-    board=genBoard(width,height)
+def turnLeft(dir):
+    dir-=1
+    if dir<0:
+        dir=3
 
-    for x in range(200):
-        workerRB(board,random.randint(2,width//20),random.randint(2,height//20))
+    return dir
 
-        show(cv2.resize(board, dsize=(20 * width, 20 * height), interpolation=cv2.INTER_AREA), wait=False)
+def turnRight(dir):
+    dir+=1
+    if dir>3:
+        dir=0
 
-def wallFollower(maze,dir,x,y):
-    #1234
-    maze[1,1]
+    return dir
+
+def straightFree(dir,maze,x,y):
+    if dir==0 and maze[y-1,x]>0:
+        return True
+    if dir==1 and maze[y,x+1]>0:
+        return True
+    if dir==2 and maze[y+1,x]>0:
+        return True
+    if dir==3 and maze[y,x-1]>0:
+        return True
+    else:
+        return False
+
+def moveStraight(dir,x,y):
+    if dir==0:
+        y-=1
+    elif dir==1:
+        x+=1
+    elif dir==2:
+        y+=1
+    elif dir==3:
+        x-=1
+
+    return x,y
+
+def turnAround(dir):
+    return (dir+2)%3
+
+def wallFollower(maze,cmaze):
+    # If left is free:
+    #     Turn Left
+    # Else if left is occupied and straight is free:
+    #     Go Straight
+    # Else if left and straight are occupied:
+    #     Turn Right
+    # Else if left/right/straight are occupied or you crashed:
+    #     Turn 180 degrees
+
     h,w=maze.shape
 
-    # at end
-    if x==w-1 and y==h-1:
-        return maze
-    elif dir==3 and maze[y,x+1]==255:
-        wallFollower(maze,3,x+1,y)
-
+    x,y=0,1
     dir=2
+    count=0
+    while cmaze[h-2,w-2,2]>0:
+        if x==w-1:
+            x-=1
+        if y==h-1:
+            y-=1
+        if leftFree(dir,maze,x,y):
+            dir=turnLeft(dir)
+            x,y=moveStraight(dir,x,y)
+            cmaze[y,x,2]-=70
+        elif straightFree(dir,maze,x,y):
+            while straightFree(dir,maze,x,y) and not leftFree(dir,maze,x,y):
+                x,y=moveStraight(dir,x,y)
+                cmaze[y,x,2]-=70
+        elif rightFree(dir,maze,x,y):
+            dir=turnRight(dir)
+            x,y=moveStraight(dir,x,y)
+            cmaze[y,x,2]-=70
+        else:
+            dir=(dir+2)%3
+            x,y=moveStraight(dir,x,y)
+            cmaze[y,x,2]-=70
+
+
+        if count%1000==0:
+            show(cv2.resize(cmaze, dsize=(20 * 1920, 20 * 1080), interpolation=cv2.INTER_AREA), wait=False)
+            print(x,y)
+        count+=1
+
 
 def tremaux(maze,cmaze):
     # Choose Random Direction
@@ -224,9 +323,61 @@ def tremaux(maze,cmaze):
             maze[y,x-1]-=1
         show(cv2.resize(cmaze, dsize=(20 * 1920, 20 * 1080), interpolation=cv2.INTER_AREA), wait=False)
 
+def mazeFollow(maze):
+    h,w=maze.shape
+    print(h,w)
 
+    cmaze=cv2.cvtColor(maze,cv2.COLOR_GRAY2BGR)
+
+    cmaze[1,1,:]=(98,96,213)
+    cmaze[h-2,w-2,:]=(11,195,236)
+    
+    video = cv2.VideoWriter("lefthand-rbSolve.avi", cv2.VideoWriter_fourcc(*"MJPG"), 30, (1920, 1080))
+
+    dir=2
+    count=0
+    x,y=1,1
+    while True:
+        if x==w-2 and y==h-2:
+            break
+        elif leftFree(dir,maze,x,y):
+            dir=turnLeft(dir)
+            x,y=moveStraight(dir,x,y)
+        elif straightFree(dir,maze,x,y):
+            x,y=moveStraight(dir,x,y)
+        elif rightFree(dir,maze,x,y):
+            dir=turnRight(dir)
+            x,y=moveStraight(dir,x,y)
+        else:
+            dir=turnAround(dir)
+        cmaze[y,x,1]-=50
+        frame=cv2.resize(cmaze, dsize=(1920, 1080), interpolation=cv2.INTER_AREA)
+        video.write(frame)
+        count+=1
+
+    show(cv2.resize(cmaze, dsize=(20 * 1920, 20 * 1080), interpolation=cv2.INTER_AREA), wait=True)
+
+maze=cv2.imread("rb-maze.png",0)
+mazeFollow(maze)
 
 # recursiveBacktracking(1920,1080)
-maze=sidewinder(1920,1080)
+# maze=recursiveBacktracking(1920,1080)
+# cmaze=cv2.cvtColor(maze,cv2.COLOR_GRAY2BGR)
+#
+# wallFollower(maze,cmaze)
 
-tremaux(maze,(cv2.cvtColor(maze,cv2.COLOR_GRAY2BGR)))
+# cap = cv2.VideoCapture("rb-maze.avi")
+#
+# out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*"MJPG"), 30, (1920,1080))
+#
+# while True:
+#     ret, frame = cap.read()
+#     if ret == True:
+#         b = cv2.resize(frame,(1920,1080),fx=0,fy=0, interpolation = cv2.INTER_AREA)
+#         out.write(b)
+#     else:
+#         break
+#
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
