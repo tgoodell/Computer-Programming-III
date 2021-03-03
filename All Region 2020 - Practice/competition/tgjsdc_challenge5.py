4c18669df9ce272d7905531281c3076767310ae9
Laptop <tgoodell@disroot.org>
2021-03-03 09:53:05 -0600

    32

--- All Region 2020 - Practice/competition/tgjsdc_challenge5.py
+++ All Region 2020 - Practice/competition/tgjsdc_challenge5.py
@@ -4,20 +4,20 @@ inputFile=open("gear_specs.csv")
 # ['Gear Train', 'Position', 'Number of Teeth', 'Diameter (cm)']
 for line in inputFile:
     (line.replace("\n","").split(","))
-    if
+    # if
 
 
 
-{
-    “Gear Train 1”: {
-        “Gear Ratio”: 1.5,
-        “Gear Train Type”: “Torque”
-    },
-    “Gear Train 2”: {
-        “Gear Ratio”: 0.5,
-        “Gear Train Type”: “Speed”
-    },
-    “Gear Train 3”: {
-        “Gear Ratio”: 1.0,
-        “Gear Train Type”: “Balanced”
-    }
+# {
+#     “Gear Train 1”: {
+#         “Gear Ratio”: 1.5,
+#         “Gear Train Type”: “Torque”
+#     },
+#     “Gear Train 2”: {
+#         “Gear Ratio”: 0.5,
+#         “Gear Train Type”: “Speed”
+#     },
+#     “Gear Train 3”: {
+#         “Gear Ratio”: 1.0,
+#         “Gear Train Type”: “Balanced”
+#     }
