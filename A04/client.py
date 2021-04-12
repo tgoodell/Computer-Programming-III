b70addeb2369fcd3810b171f49239b25423c8fe8
Laptop <tgoodell@disroot.org>
2021-04-12 09:56:41 -0500

    60

--- A04/client.py
+++ A04/client.py
@@ -1,13 +1,8 @@
 import socket, re
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 50000        # The port used by the server
-
-wordsByLength={}
-for line in open("dictionary.txt"):
-    word=line.strip()
-    if len(word) not in wordsByLength:
-        wordsByLength[len(word)]=[]
-    wordsByLength[len(word)].append(word);
+import random
+import time
 
 def genNewDictionary(dictionary,misses,node,guess=""):
     newDict=[]
@@ -16,12 +11,12 @@ def genNewDictionary(dictionary,misses,node,guess=""):
 
         for miss in misses:
             miss=miss[0]
-            if miss in word:
+            if miss.upper() in word.upper():
                 bad=True
                 break
         if not bad:
-            for lettern,letter in zip(node,word):
-                if lettern=="_" or letter==lettern or letter==guess:
+            for lettern,letter in zip(node.upper(),word.upper()):
+                if lettern=="_" or letter==lettern or letter==guess.upper():
                     pass
                 else:
                     bad=True
@@ -32,14 +27,13 @@ def genNewDictionary(dictionary,misses,node,guess=""):
 
 def maximizer(node,dictionary,misses,hits):
     alpha={"a","b","c","d","e","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"}
-    # alpha = cutAlpha(dictionary, alpha)
-    # tdictionary=[]
+
     if misses:
         for n in misses:
             if n in alpha:
                 alpha.remove(n)
     bestSize=0
-    bestGuess="-1"
+    bestGuess=""
     for letter in alpha:
         _,newSize=genNewDictionary(dictionary,misses,node,guess=letter)
         bestSize=max(newSize,bestSize)
@@ -54,81 +48,151 @@ def minimizer(node,dictionary,misses,hits):
             if n in alpha:
                 alpha.remove(n)
     bestSize=100000000
-    bestGuess="-1"
+    bestGuess=""
     for letter in alpha:
         _,newSize=genNewDictionary(dictionary,misses,node,guess=letter)
         bestSize=min(newSize,bestSize)
         if bestSize==newSize and letter not in hits:
             bestGuess=letter
-    return bestGuess
+        # print(bestGuess)
+    return bestGuess.upper()
+
+def negamax(node,depth,a=-100000,b=100000):
+    if depth==0:
+        return node.value(),None
+    plays = [*node.getPlays()]
+    if node.isHangmanTurn() and len(plays)==1 and "_" not in plays[0]: # done # terminal node
+        return -10000000-depth, None
+    # bestPlays=plays[0]
+    bestV=-1000000
+    for play in plays: # for every available play from the node
+        child=node.play(play) # Go down to next game.
+        v=-negamax(child.value(),depth-1,-b,-a)[0] # Get eval for the next game.
+        if v>a:
+            a=v
+        if v>=bestV:
+            bestV=v
+            bestPlay=play
+        if a>=b:
+            break
+    return bestV,bestPlay
+
+# iterative deepening
+def deepen(node):
+    depth=1
+    best=None
+    while True:
+        start=time.time()
+        negamax(node,depth)
+        end=time.timte()
+        if end-start>1:
+            break
+        depth+=1
+    return [*best,depth]
 
 def minimax(node,depth,maxer,dictionary,misses,hits):
+    alpha={"a","b","c","d","e","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"}
+    if misses:
+        for n in misses:
+            if n in alpha:
+                alpha.remove(n)
+    
     if depth==0 or len(dictionary)==1:
-        return 0
+        return -1
     if maxer:
-        bestGuess=maximizer(node,dictionary,misses,hits)
-        dict,_=genNewDictionary(dictionary,misses,node,guess=bestGuess)
-        minimax(node,depth-1,False,dict,misses,hits)
+        bestSize = 0
+        bestGuess = ""
+        for letter in alpha:
+            guess, bSize = minimax(node,depth-1,False,dictionary,misses,hits)
+            bestSize=max(guess, bestSize)
+            if bSize==bestSize and letter not in hits:
+                bestGuess = letter
+        dict,size=genNewDictionary(dictionary,misses,node,guess=bestGuess)
 
     else:
-        bestGuess=minimizer(node, dictionary, misses, hits)
-        dict,_=genNewDictionary(dictionary,misses,node,guess=bestGuess)
-        minimax(node,depth-1,True,dict,misses,hits)
-    return bestGuess
-
-def getWordGroup(progress,currentDict):
-    newDict={}
-    for word in currentDict:
-        bad=False
-        for letter,cletter in word,progress:
-            if cletter!="_" and cletter==letter:
-                pass
-            else:
-                bad=True
-        if not bad:
-            newDict.append(word)
-
-    return newDict
+        bestSize = 1000000000
+        bestGuess = ""
+        for letter in alpha:
+            guess, bSize = minimax(node, depth - 1, True, dictionary, misses, hits)
+            bestSize = min(guess, bestSize)
+            if bSize == bestSize and letter not in hits:
+                bestGuess = letter
+        dict, size = genNewDictionary(dictionary, misses, node, guess=bestGuess)
+    return bestGuess,size
 
 def sepMissHit(node,guesses):
     hits=[]
     misses=[]
     for check in guesses:
         if check in node:
-            hits.append(check)
+            hits.append(check.lower())
         else:
-            misses.append(check)
+            misses.append(check.lower())
 
     return misses,hits
 
-def hangman(data,dictionary):
+def guessLetter(node,word,guess):
+    for lettern,letter in zip(node,word):
+        if letter==guess:
+            node=node[0:word.index(letter)] + letter + node[word.index(letter) + 1:]
+    return node.upper()
+
+def hangman(data,dictionary,sourceDict,currentWord):
     word,guesses,guess=re.findall(b"(\w+) \[(\w*),(\w*)\]".decode("utf-8"),data.decode("utf-8"))[0]
     misses,hits=sepMissHit(word,guesses)
-    dict,_=genNewDictionary(dictionary,misses,word,guess=guess)
-    bestGuess=minimax(guess,26,True,dict,misses,hits)
 
-    if bestGuess==guess:
-        return word+" "+guesses
-    
-    return bestReturn(word,guesses,guess)[0]
+    if len(misses)>8:
+        return "Hangee Lost.",dictionary,currentWord
 
-def hangee(data):
+    dict,_ = genNewDictionary(sourceDict[len(word)], guesses, word.lower())
+        
+    bestGuess=minimax(word,3,True,dict,misses,hits)
+
+    if len(dict)>0:
+        currentWord=dict[random.randint(0,len(dict)-1)]
+
+    print(currentWord + " " + guesses)
+    if bestGuess==-1:
+        return "Hangman Lost.",dict,currentWord
+    elif bestGuess.upper()==guess.upper():
+        return guessLetter(word.lower(),currentWord,bestGuess).upper(),dict,currentWord
+    else:
+        return word,dict,currentWord
+
+def hangee(data,dictionary,sourceDict):
     word,guesses=re.findall(b"(\w+) \[(\w*)]".decode("utf-8"),data.decode("utf-8"))[0]
+    misses,hits=sepMissHit(word,guesses)
 
+    # if len(guesses)==0:
 
-# ~ print(hangman(b"TOAD_ [ABCSREILNUOGDT,Y]"))
-# ~ print(hangee(b"TOAD_ [ABCSREILNUOGDT]"))
+    dict, _ = genNewDictionary(sourceDict[len(word)], misses, word.lower())
 
+    bestGuess=minimax(word,3,False,dict,misses,hits)
 
+    return bestGuess.upper(),dict
+
+sourceDict={}
+for line in open("Collins Scrabble Words (2019).txt"):
+    word=line.strip()
+    if len(word) not in sourceDict:
+        sourceDict[len(word)]=[]
+    sourceDict[len(word)].append(word);
+
+dictionary=[]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+firstTime=True
+currentWord=""
+
 while True:
     data = s.recv(1024)
-    x=""
     if(b"," in data):
-        x=hangman(data)
+        x,dictionary,currentWord=hangman(data,dictionary,sourceDict,currentWord)
+        print(x + " " + str(len(dictionary))+"\n")
     else:
-        x=hangee(data)
+        x,dictionary=hangee(data,dictionary,sourceDict)
+        print(x + " " + str(len(dictionary))+"\n")
     # ~ print(data)
     s.sendall(x.encode())
--- A04/server.py
+++ A04/server.py
@@ -1,4 +1,3 @@
-                                                                                                                                                                     
 
 import socket               # Import socket module
 
@@ -13,22 +12,22 @@ print('Waiting for clients...')
 
 s.bind((host, port))        # Bind to the port
 s.listen()                 # Now wait for client connection.
-conn1,addr1=s.accept()
-conn2,addr2=s.accept()
+conn1 ,addr1=s.accept()
+conn2 ,addr2=s.accept()
 word=b"_____"
 guesses=b""
 while True:
-    conn1.sendall(b"%s [%s]"%(word,guesses))
+    conn1.sendall(b"%s [%s] " %(word ,guesses))
     guess = conn1.recv(1024)
-    print(b"%s [%s,%s]"%(word,guesses,guess))
-    conn2.sendall(b"%s [%s,%s]"%(word,guesses,guess))
+    print(b"%s [%s,%s] " %(word ,guesses ,guess))
+    conn2.sendall(b"%s [%s,%s] " %(word ,guesses ,guess))
     word = conn2.recv(1024)
     guesses+=guess
     if b"_" not in word:
-        word=b"_"*random.randint(2,12)
-        guesses=b""
-    
-    print(b"%s [%s]"%(word,guesses))
-    
+        word =b"_" *random.randint(2 ,12)
+        guesses =b""
+
+    print(b"%s [%s] " %(word ,guesses))
+
 
 s.close()
